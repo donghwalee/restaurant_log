e60eb07d931cf443a0a66831380501b000c71537
Donghwa Lee <donghwalee328@gmail.com>
2015-11-29 08:55:44 -0500

    post with hard coded values work

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -1,11 +1,32 @@
 var app = angular.module('RestaurantApp', []);
 
+
+// Header
 app.controller('HeaderController', ['$http', function($http){
-
   var controller = this;
-
+  // Get current user from route
   $http.get('/session').success(function(data){
     controller.current_user = data.current_diner;
-    console.log(controller.current_user);
   });
+}]);
+
+
+// Main
+app.controller('RestaurantController', ['$http', function($http){
+  var token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  this.createRestaurant = function(){
+    $http.post('/restaurants', {
+      authenticity_token: token,
+      restaurant: {
+        name: "ABC Hard",
+        rest_type: "Japanese",
+        address: "NYC",
+        dined_at: "11-11-2011",
+        rating: 4,
+        comments: "Great sushi"
+      }
+    }).success(function(data){
+      console.log(data);
+    });
+  }
 }]);
